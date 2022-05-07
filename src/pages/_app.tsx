c16ce3e6f33e944c76b03c31b8898d0121405fa0
reecejohnson <reece.robert.johnson@gmail.com>
2022-05-07 11:17:22 +0100

    feat: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,6 +41,10 @@ export default function App({
       >
         <Head>
           <title>RainbowKit Wallet Connection</title>
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌈</text></svg>"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap"
