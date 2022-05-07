1fc16560287e53ef81c703e37df6fd23d7dbc5b3
reecejohnson <reece.robert.johnson@gmail.com>
2022-05-07 11:22:56 +0100

    feat: responsvie styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen items-center justify-center gap-8">
       <div className="flex flex-col gap-4 items-center">
-        <h1 className="text-5xl tracking-[-1px] font-semibold text-blue-600">
+        <h1 className="text-2xl tracking-[-1px] font-semibold text-blue-600 text-center md:text-5xl">
           RainbowKit Wallet Connection 🌈
         </h1>
-        <p className="text-gray-700 text-xl">
+        <p className="text-gray-700 text-base text-center md:text-xl">
           Example application integrating wallet connection with{" "}
           <a href="https://www.rainbowkit.com/" target="_blank">
             RainbowKit
